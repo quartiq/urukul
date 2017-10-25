a3d5b6010dd345c0d7a83f4c5f0570adf0fd2b00
Robert Jordens <jordens@gmail.com>
2017-10-25 18:55:40 +0200

    urukul pins
    from schematics
    https://github.com/m-labs/sinara/commit/14ec15d50d8798e52407c825f3c508957eebdeee

--- test_cpld.py
+++ test_cpld.py
@@ -3,11 +3,11 @@ from migen import *
 
 class Top(Module):
     def __init__(self, platform):
-        scki = platform.request("scki")
+        scki = platform.request("eem", 0).io
         self.clock_domains.cd_sys = cd_sys = ClockDomain("sys", reset_less=True)
         self.comb += self.cd_sys.clk.eq(scki)
 
-        led = platform.request("user_led")
+        led = platform.request("dds", 0).led[0]
         self.sync += led.eq(~led)
 
 
