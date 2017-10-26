83b8b1e13295e99c7270a51b73b98864c303455e
Robert Jordens <jordens@gmail.com>
2017-10-26 15:56:08 +0200

    urukul logic draft complete

--- urukul.py
+++ urukul.py
@@ -1,42 +1,70 @@
 from migen import *
 
 
+class SR(Module):
+    def __init__(self, width):
+        self.sdi = Signal()
+        self.sdo = Signal()
+        self.sel = Signal()
+
+        self.data = Signal(width)
+
+        # # #
+
+        sr = Signal(width)
+        cnt = Signal(max=width, reset=width-1)
+        cnt_done = Signal()
+
+        self.comb += [
+                self.sdo.eq(sr[-1]),
+                cnt_done.eq(cnt == 0),
+        ]
+        i = Signal()
+        self.sync.sck1 += [
+                If(self.sel,
+                    i.eq(self.sdi)
+                )
+        ]
+        self.sync.sck0 += [
+                If(cnt_done,
+                    cnt.eq(cnt.reset),
+                    self.data.eq(sr),
+                ).Elif(self.sel,
+                    sr.eq(Cat(i, sr)),
+                    cnt.eq(cnt - 1)
+                )
+        ]
+
+
 class CFG(Module):
     def __init__(self, platform, n=4):
-        self.write = Record([
+        self.cfg = Record([
             ("rf_sw", n),
             ("led", n),
             ("profile", 3),
-            ("in_sel", 1),
-            ("reset", 1),
-            ("io_reset", 1),
-            ("master_reset", 1),
+
+            ("att_le", 1),
+
+            ("clk_sel", 1),
+            ("sync_sel", 1),
+
+            ("dds_rst", 1),
+            ("io_rst", 1),
             ("att_rst", 1),
-            ("smp_err", n),
-            ("pll_lock", n),
         ])
-        self.read = Record(self.write.layout)
         dds_common = platform.lookup_request("dds_common")
+        dds_sync = platform.lookup_request("dds_sync")
         att = platform.lookup_request("att")
-        in_sel = platform.request("in_sel")
+        clk = platform.lookup_request("clk")
         self.comb += [
-                dds_common.profile.eq(self.write.profile),
-                self.read.profile.eq(dds_common.profile),
-
-                in_sel.eq(self.write.in_sel),
-                self.read.in_sel.eq(in_sel),
-
-                dds_common.reset.eq(self.write.reset),
-                self.read.reset.eq(dds_common.reset),
-
-                dds_common.reset.eq(self.write.io_reset),
-                self.read.reset.eq(dds_common.io_reset),
-
-                dds_common.master_reset.eq(self.write.master_reset),
-                self.read.master_reset.eq(dds_common.master_reset),
-
-                att.rst_n.eq(~self.write.att_rst),
-                self.read.att_rst.eq(~att.rst_n),
+                dds_common.profile.eq(self.cfg.profile),
+                clk.in_sel.eq(self.cfg.clk_sel),
+                dds_sync.sync_sel.eq(self.cfg.sync_sel),
+                dds_common.reset.eq(self.cfg.dds_rst),
+                dds_common.master_reset.eq(dds_common.reset),
+                dds_common.io_reset.eq(self.cfg.io_rst),
+                att.rst_n.eq(~self.cfg.att_rst),
+                att.le.eq(self.cfg.att_le),
         ]
 
         for i in range(n):
@@ -44,44 +72,102 @@ class CFG(Module):
             dds = platform.lookup_request("dds", i)
             self.comb += [
                     sw.oe.eq(0),
-                    dds.rf_sw.eq(sw.io ^ self.write.rf_sw[i]),
-                    self.read.rf_sw[i].eq(dds.rf_sw),
-                    dds.led[0].eq(dds.rf_sw),
-
-                    dds.led[1].eq(self.write.led[i]),
-                    self.read.led[i].eq(dds.led[1]),
-
-                    self.read.smp_err[i].eq(dds.smp_err),
-
-                    self.read.pll_lock[i].eq(dds.pll_lock),
+                    dds.rf_sw.eq(sw.io ^ self.cfg.rf_sw[i]),
+                    dds.led[0].eq(dds.rf_sw),  # green
+                    dds.led[1].eq(self.cfg.led[i] | dds.smp_err | ~dds.pll_lock),
             ]
-
 
 
 class Top(Module):
     def __init__(self, platform):
-        self.clock_domains.cd_sys = ClockDomain("sys", reset_less=True)
-        sck = platform.request("eem", 0)
-        self.comb += [
-                sck.oe.eq(0),
-                self.cd_sys.clk.eq(sck.io),
-        ]
-
-        ifc_mode = platform.request("ifc_mode")
+        clk = platform.request("clk")
+        dds_sync = platform.request("dds_sync")
         dds_common = platform.request("dds_common")
+        ifc_mode = platform.request("ifc_mode")
         att = platform.request("att")
         dds = [platform.request("dds", i) for i in range(4)]
-        # att
-        # sr
-        # cs mux
-        # ifc mode
-        # spi, io_update, io_update ret
-        # sync routing, dds_common
 
-        self.submodules += CFG(platform)
+        ts_clk_div = TSTriple()
+        self.specials += [
+                ts_clk_div.get_tristate(clk.div)
+        ]
 
-        tp = [platform.request("tp", i) for i in range(5)]
-        self.sync += tp[0].eq(~tp[0])
+        eem = []
+        for i in range(12):
+            tsi = TSTriple()
+            eemi = platform.request("eem", i)
+            self.specials += tsi.get_tristate(eemi.io)
+            self.comb += eemi.oe.eq(tsi.oe)
+            eem.append(tsi)
+
+        # AD9910 only
+        self.clock_domains.cd_sys = ClockDomain("sys", reset_less=True)
+        self.clock_domains.cd_sck1 = ClockDomain("sck0", reset_less=True)
+        self.clock_domains.cd_sck0 = ClockDomain("sck1", reset_less=True)
+
+        sck1, nu_sck = Signal(), Signal()
+        self.specials += [
+                Instance("CLK_DIV2", i_CLKIN=dds_sync.clk0,
+                    o_CLKDV=self.cd_sys.clk),
+                Instance("BUFG", i_I=eem[7].i, o_O=nu_sck),
+                Instance("BUFG", i_I=eem[0].i, o_O=sck1),
+        ]
+
+        en_9910 = Signal()  # AD9910 populated (instead of AD9912)
+        en_nu = Signal()  # NU-Servo operation with quad SPI
+        en_eemb = Signal()  # EEM-B connected
+        en_unused = Signal()
+        self.comb += Cat(en_9910, en_nu, en_eemb, en_unused).eq(ifc_mode)
+
+        self.comb += [
+                [eem[i].oe.eq(0) for i in range(12) if i not in (2, 10)],
+                eem[2].oe.eq(~en_nu),
+                eem[10].oe.eq(~en_nu & en_eemb),
+                eem[10].o.eq(eem[6].i),
+                self.cd_sck1.clk.eq(Mux(en_nu, nu_sck, sck1)),
+                self.cd_sck0.clk.eq(~self.cd_sck1.clk),
+                dds_sync.clk_out_en.eq(~en_nu & en_eemb & en_9910),
+                dds_sync.sync_out_en.eq(~en_nu & en_eemb & en_9910),
+                # 1: div-by-4 for AD9910
+                # z: div-by-1 for AD9912
+                ts_clk_div.oe.eq(~en_9910),
+                ts_clk_div.o.eq(1),
+        ]
+
+        cfg = CFG(platform)
+        sr = SR(len(cfg.cfg))
+        self.submodules += cfg, sr
+
+        sel = Signal(8)
+        self.comb += [
+                Array(sel)[Cat(eem[3].i, eem[4].i, eem[5].i)].eq(1),
+                att.clk.eq(sel[3] & sck1),
+                att.s_in.eq(eem[1].i),
+                If(sel[3],
+                    eem[2].o.eq(att.s_out)),
+
+                sr.sel.eq(sel[1]),
+                sr.sdi.eq(eem[1].i),
+                If(sel[1],
+                    eem[2].o.eq(sr.sdo)),
+
+                cfg.cfg.raw_bits().eq(sr.data)
+        ]
+        for seli, ddsi, nu_mosi in zip(sel[4:], dds, eem[8:]):
+            self.comb += [
+                    ddsi.cs_n.eq(~(seli | (en_nu & eem[2].i))),
+                    ddsi.sck.eq(Mux(en_nu & ~seli, nu_sck, sck1)),
+                    ddsi.sdi.eq(Mux(en_nu & ~eem[5].i, nu_mosi.i,
+                        eem[1].i)),
+                    If(seli,
+                        eem[2].o.eq(ddsi.sdo)),
+                    ddsi.io_update.eq(eem[6].i),
+            ]
+
+        for ddsi in dds:
+            self.comb += platform.request("tp").eq(ddsi.smp_err)
+        self.comb += platform.request("tp").eq(
+                ~Cat([ddsi.pll_lock for ddsi in dds]) == 0)
 
 
 def main():
