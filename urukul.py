2c9c8304953c2edf4798677864e10523d8dfc53b
Robert Jordens <jordens@gmail.com>
2017-11-21 09:37:16 +0100

    cleanup doc

--- urukul.py
+++ urukul.py
@@ -4,181 +4,6 @@ from migen import *
 # increment this if the behavior (LEDs, registers, EEM pins) changes
 __proto_rev__ = 5
 
-"""
-Urukul IO router and configuration/status
-=========================================
-
-Pin Out
--------
-
-Urukul operates from one or two EEM connectors.
-NU-Servo mode requires two EEM connectors.
-Standard SPI mode only needs the second EEM connector to interface with
-high resolution RF switching and synchronization signals.
-The complete Urukul functionality can be accessed controlled using only the
-Standard SPI interface.
-
-| EEM  | LVDS index | PCB net | function               |
-|------+------------+---------+------------------------|
-| EEM0 | 0          | A0      | SCLK                   |
-| EEM0 | 1          | A1      | MOSI                   |
-| EEM0 | 2          | A2      | MISO/NU_CLK            |
-| EEM0 | 3          | A3      | CS0                    |
-| EEM0 | 4          | A4      | CS1                    |
-| EEM0 | 5          | A5      | CS2/NU_CS              |
-| EEM0 | 6          | A6      | IO_UPDATE              |
-| EEM0 | 7          | A7      | DDS_RESET/SYNC_OUT     |
-| EEM1 | 0          | B8      | SYNC_CLK/NU_MOSI0      |
-| EEM1 | 1          | B9      | SYNC_IN/NU_MOSI1       |
-| EEM1 | 2          | B10     | IO_UPDATE_RET/NU_MOSI2 |
-| EEM1 | 3          | B11     | NU_MOSI3               |
-| EEM1 | 4          | B12     | SW0                    |
-| EEM1 | 5          | B13     | SW1                    |
-| EEM1 | 6          | B14     | SW3                    |
-| EEM1 | 7          | B15     | SW4                    |
-
-
-IFC_MODE
---------
-
-The four IFC mode switches are assigned as:
-
-| IFC_MODE | name    | function                                        |
-|----------+---------+-------------------------------------------------|
-| 0        | EN_9910 | On if AD9910 is populated                       |
-| 1        | EN_NU   | On if NU-Servo mode is used                     |
-| 2        | EN_EEM1 | On if the SYNC signals on EEM1 should be driven |
-| 3        | UNUSED  | Unused switch                                   |
-
-See :class:`Urukul`
-
-SPI
----
-
-An SPI interface is provided to access any of the six serial devices (the
-configuration/status SPI interface, the attenuator SPI interface,
-the four DDS SPI interfaces). It comprises the SCLK, MOSI, MISO, CS0, CS1,
-CS2 signal. With EN_NU, MISO and CS2 (and the functionality provided by them)
-is unavailable. Since CS2 is unavailable in EN_NU, CS >= 4 (the individual
-DDS) are only available outside of EN_NU or through CS = 3 (and CFG.MASK_NU).
-
-The target chip (or set of chips) is selected by CS0/CS1/CS2 (CS2 being the
-MSB). The encoding is as follows:
-
-| CS      | chip                                       |
-|---------+--------------------------------------------|
-| 0=0b000 | None                                       |
-| 1=0b001 | CFG                                        |
-| 2=0b010 | ATT                                        |
-| 3=0b011 | Multiple DDS (those masked by CFG.MASK_NU) |
-| 4=0b100 | DDS0                                       |
-| 5=0b101 | DDS1                                       |
-| 6=0b110 | DDS2                                       |
-| 7=0b111 | DDS3                                       |
-
-The SPI interface is CPOL=0, CPHA=0, SPI mode 0, 4 wire. Clock cycles during
-CS[0:2] = 0 are ignored.
-
-See :class:`Urukul` and :class:`SR`
-
-CFG
----
-
-The configuration status register controls the overall operation of Urukul,
-allows some configuration options to be changed and the status of some signals
-to be monitored.
-
-It is 24 bits wide, MSB first.
-
-See :class:`SR`
-
-CFG write
-.........
-
-See :class:`CFG`
-
-CFG read
-........
-
-See :class:`Status`
-
-QSPI
-----
-
-If EN_NU is activated, the four DDS are additionally exposed through a
-quad-SPI write-only interface defined by the signals NU_CLK, NU_CS, and
-NU_MOSI[0:3].
-
-Only those DDS which are not masked by CFG.MASK_NU can be accessed through the
-QSPI interface. This allows initial setup and configuration of the DDS
-individually through the "regular" SPI interface in EN_NU mode.
-
-DDS[0:3].CS is driven by NU_CS (for those DDS not masked)
-DDS[0:3].SCK is driven by NU_CLK (for those DDS not masked)
-DDS[0:3].MOSI is driven by NU_MOSI[0:3] (for those DDS not masked)
-DDS[0:3].MISO is unavailable
-DDS[0:3].IO_UPDATE is driven by IO_UPDATE (for those DDS not masked)
-
-See :class:`Urukul`
-
-ATT
----
-
-The digital step attenuators are daisy-chained (ATT.S_OUT driving the next
-ATT.S_IN) and form a 32 bit SPI compatible shift register. The data from the
-shift register is transferred to the active attenuation register on the rising
-edge of CFG.ATT_LE.
-
-
-Clocking
---------
-
-CFG.CLK_SEL selects the clock source for the clock fanout to the DDS.
-
-When EN_9910 is on, the clock to the DDS (from the XCO, the internal MMCX or
-the external SMA) is divided by 4.
-
-
-Synchronization
----------------
-
-DDS_RESET (not EN_9910) and SYNC_OUT (EN_9910) share an EEM signal.
-DDS_RESET provides a way to deterministically reset all AD9912 DDS SYNC_CLK
-divider. (https://ez.analog.com/docs/DOC-14472)
-
-SYNC_OUT is an input to the SYNC fanout (input to Urukul) to externally
-and actively synchronize the AD9910 SYNC_CLK dividers. The SYNC fanout can
-be driven using either EEM1.SYNC_OUT or DDS0.SYNC_OUT (selected by
-CFG.CLK_SEL).
-
-SYNC_CLK and SYNC_IN are available with EN_9910 to synchronize external logic
-to the DDS. A round-trip time measurement using IO_UPDATE_RET would need to be
-performed. SYNC_IN is an output from Urukul and an input to the controlling
-FPGA and an input to all DDS.
-
-IO_UPDATE_RET is provided to determine the round trip time for IO_UPDATE.
-
-
-RF switches
------------
-
-The RF switches are activated with CFG.RF_SW or (logic OR) SW[0:3].
-
-LEDs
-----
-
-The green channel LEDs mirror the status of the RF switches.
-The red LEDs are activated by ``CFG.LED | (EN_9910 & (DDS[0:3].SMP_ERR |
-~DDS[0:3].PLL_LOCK))``. I.e. they are lit by the register or an
-synchronization/PLL error on that channel's DDS.
-
-Test points
------------
-
-The test points expose miscellaneous signals for debugging and are not part
-of the protocol revision.
-"""
-
 
 class SR(Module):
     """
@@ -351,9 +176,11 @@ class Status(Module):
 
 
 class Urukul(Module):
-    """Urukul CPLD logic router
+    """
+    Urukul IO router and configuration/status
+    =========================================
 
-    The CPLD controls/monitors
+    The CPLD controls/monitors:
 
     * the four AD9912 or AD9910 DDS (SPI, status, reset, IO update)
     * the four digitally controlled RF step attenuators (SPI, ATT_LE, reset)
@@ -365,6 +192,175 @@ class Urukul(Module):
     * the two EEM connectors
     * the test pads
     * the four configuration switches
+
+    Pin Out
+    -------
+
+    Urukul operates from one or two EEM connectors. In standard SPI mode, the
+    complete Urukul functionality can be accessed using only that interface.
+    Standard SPI mode only needs the second EEM connector to interface with
+    high resolution RF switching and synchronization signals. NU-Servo mode
+    always requires two EEM connectors.
+
+    | EEM  | LVDS pair | PCB net | Function                |
+    |------+-----------+---------+-------------------------|
+    | EEM0 | 0         | A0      | SCLK                    |
+    | EEM0 | 1         | A1      | MOSI                    |
+    | EEM0 | 2         | A2      | MISO, NU_CLK            |
+    | EEM0 | 3         | A3      | CS0                     |
+    | EEM0 | 4         | A4      | CS1                     |
+    | EEM0 | 5         | A5      | CS2, NU_CS              |
+    | EEM0 | 6         | A6      | IO_UPDATE               |
+    | EEM0 | 7         | A7      | DDS_RESET, SYNC_OUT     |
+    | EEM1 | 0         | B8      | SYNC_CLK, NU_MOSI0      |
+    | EEM1 | 1         | B9      | SYNC_IN, NU_MOSI1       |
+    | EEM1 | 2         | B10     | IO_UPDATE_RET, NU_MOSI2 |
+    | EEM1 | 3         | B11     | NU_MOSI3                |
+    | EEM1 | 4         | B12     | SW0                     |
+    | EEM1 | 5         | B13     | SW1                     |
+    | EEM1 | 6         | B14     | SW3                     |
+    | EEM1 | 7         | B15     | SW4                     |
+
+    IFC_MODE
+    --------
+
+    DIP switches are used to configure the operation of the Urukul CPLD. The
+    four IFC mode switches are assigned as:
+
+    | IFC_MODE | Name    | Function                                        |
+    |----------+---------+-------------------------------------------------|
+    | 0        | EN_9910 | On if AD9910 is populated                       |
+    | 1        | EN_NU   | On if NU-Servo mode is used                     |
+    | 2        | EN_EEM1 | On if the SYNC signals on EEM1 should be driven |
+    | 3        | UNUSED  | Unused switch                                   |
+
+    See :class:`Urukul`
+
+    SPI
+    ---
+
+    An SPI interface is provided to access any of the six serial devices (the
+    configuration/status SPI interface, the attenuator SPI interface, and the
+    four DDS SPI interfaces). It comprises the SCLK, MOSI, MISO, CS0, CS1, and
+    CS2 signals. With EN_NU, both MISO and CS2 (and the functionality provided
+    by them) are unavailable. I.e. CS >= 4 (the individual DDS access) are only
+    available outside of EN_NU or through CS = 3 (and CFG.MASK_NU).
+
+    The target chip (or set of chips) is selected by CS0/CS1/CS2 (CS2 being the
+    MSB). The encoding is as follows:
+
+    | CS        | chip                                       |
+    |-----------+--------------------------------------------|
+    | 0 = 0b000 | None                                       |
+    | 1 = 0b001 | CFG                                        |
+    | 2 = 0b010 | ATT                                        |
+    | 3 = 0b011 | Multiple DDS (those masked by CFG.MASK_NU) |
+    | 4 = 0b100 | DDS0                                       |
+    | 5 = 0b101 | DDS1                                       |
+    | 6 = 0b110 | DDS2                                       |
+    | 7 = 0b111 | DDS3                                       |
+
+    The SPI interface is CPOL=0, CPHA=0, SPI mode 0, 4-wire, full fuplex. Clock
+    cycles during CS[0:2] = 0 are ignored (but may still be visible on the DDS
+    SCK outputs).
+
+    See :class:`Urukul` and :class:`SR`
+
+    CFG
+    ---
+
+    The configuration status register controls the overall operation of Urukul,
+    allows some configuration options to be changed and the status of some
+    signals to be monitored.
+
+    It is 24 bits wide, MSB first.
+
+    See :class:`SR`
+
+    CFG write
+    .........
+
+    See :class:`CFG`
+
+    CFG read
+    ........
+
+    See :class:`Status`
+
+    QSPI
+    ----
+
+    If EN_NU is activated, the four DDS are additionally exposed through a
+    quad-SPI write-only interface defined by the signals NU_CLK, NU_CS, and
+    NU_MOSI[0:3].
+
+    Only those DDS which are **not** masked by CFG.MASK_NU can be accessed
+    through the QSPI interface. This allows initial setup and configuration of
+    the DDS individually through the "regular" SPI interface in EN_NU mode.
+
+    DDS[0:3].CS is driven by NU_CS (for those DDS not masked)
+    DDS[0:3].SCK is driven by NU_CLK (for those DDS not masked)
+    DDS[0:3].MOSI is driven by NU_MOSI[0:3] (for those DDS not masked)
+    DDS[0:3].MISO is unavailable
+    DDS[0:3].IO_UPDATE is driven by IO_UPDATE (for those DDS not masked)
+
+    See :class:`Urukul`
+
+    ATT
+    ---
+
+    The digital step attenuators are daisy-chained (ATT[n].S_OUT driving the
+    next ATT[n+1].S_IN) and form a 32 bit SPI compatible shift register. The
+    data from the shift register is transferred to the active attenuation
+    register on the rising edge of CFG.ATT_LE.
+
+    Clocking
+    --------
+
+    CFG.CLK_SEL selects the clock source for the clock fanout to the DDS.
+
+    When EN_9910 is on, the clock to the DDS (from the XCO, the internal MMCX
+    or the external SMA) is divided by 4.
+
+    Synchronization
+    ---------------
+
+    IO_UPDATE_RET is provided to determine the round trip time for IO_UPDATE.
+
+    DDS_RESET (not EN_9910) and SYNC_OUT (EN_9910) share an EEM signal.
+    DDS_RESET provides a way to deterministically reset all AD9912 DDS SYNC_CLK
+    divider. (https://ez.analog.com/docs/DOC-14472)
+
+    SYNC_OUT is an input to the SYNC fanout (input to Urukul, output from the
+    controlling FPGA upstream) to externally and actively synchronize the
+    AD9910 SYNC_CLK dividers. The SYNC fanout can be driven using either
+    EEM1.SYNC_OUT or DDS0.SYNC_OUT (selected by CFG.SYNC_SEL).
+
+    SYNC_CLK and SYNC_IN are available with EN_9910 & EN_EEM1 to synchronize
+    external logic to the DDS. A round-trip time measurement using
+    IO_UPDATE_RET would need to be performed. SYNC_IN is an output from Urukul,
+    an input to the controlling upstream FPGA, and an input to all DDS.
+
+    RF switches
+    -----------
+
+    The RF switches are activated with CFG.RF_SW or (logic OR) SW[0:3].
+    EEM1.SW[0:3] provide a high resolution and high-bandwidth port to RF
+    switching.
+
+    LEDs
+    ----
+
+    The green channel LEDs mirror the status of the RF switches. The red LEDs
+    are activated by ``CFG.LED | (EN_9910 & (DDS[0:3].SMP_ERR |
+    ~DDS[0:3].PLL_LOCK))``. I.e. they are lit by the register or (logic OR) an
+    synchronization/PLL error on that channel's DDS.
+
+    Test points
+    -----------
+
+    The test points expose miscellaneous signals for debugging and are not part
+    of the protocol revision.
     """
     def __init__(self, platform):
         clk = platform.request("clk")
