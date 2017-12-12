4689caf2056fb38b0e8d7c762a2bb266555abcc7
Robert Jordens <jordens@gmail.com>
2017-12-12 11:53:10 +0100

    [WIP] guess more the flash commands

--- flash.cmd
+++ flash.cmd
@@ -1,9 +1,9 @@
 setMode -bscan
 setCable -p auto
 # setCableSpeed -speed 6000000
-# addDevice -p 1 -file build/pdq_3ch.bit
 readIdCode -p 1
-# attachFlash -p 1 -spi AT45DB161D
-# assignfiletoattachedflash -p 1 -file build/pdq_3ch.mcs
-# program -e -v -p 1 -dataWidth 1 -spionly -loadfpga
+identify -inferir
+identifyMPM
+assignFile -p 1 -file build/urukul.jed
+program -e -v -p 1
 quit
