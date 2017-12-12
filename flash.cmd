56580162d9e771fd414914cbae2b579985c975bb
Robert Jordens <jordens@gmail.com>
2017-12-12 11:24:35 +0000

    fix flashing

--- flash.cmd
+++ flash.cmd
@@ -1,9 +1,9 @@
 setMode -bscan
 setCable -p auto
 # setCableSpeed -speed 6000000
-readIdCode -p 1
 identify -inferir
 identifyMPM
+readIdCode -p 1
 assignFile -p 1 -file build/urukul.jed
 program -e -v -p 1
 quit
