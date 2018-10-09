f19c91ae10f64b8b0465a44c9ca84dde594d56fb
Robert Jördens <rj@quartiq.de>
2018-10-09 15:17:08 +0000

    flash: increase sleep

--- flash_xc3.sh
+++ flash_xc3.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 /sbin/fxload -t fx2 -I /opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xusb_xp2.hex -D /dev/bus/usb/001/*`cat /sys/bus/usb/devices/1-3/devnum`
-sleep 10
+sleep 7
 ../xc3sprog/build/xc3sprog -c xpc -m /opt/Xilinx/14.7/ISE_DS/ISE/xbr/data -v build/urukul.jed:w
