2112118cd4d98fbda1d89d85b71929d934378813
Robert Jördens <rj@quartiq.de>
2019-09-11 12:29:25 +0000

    udpdate bus

--- flash_xc3.sh
+++ flash_xc3.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-/sbin/fxload -t fx2 -I /opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xusb_xp2.hex -D /dev/bus/usb/001/*`cat /sys/bus/usb/devices/1-3/devnum`
+/sbin/fxload -t fx2 -I /opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xusb_xp2.hex -D /dev/bus/usb/001/*`cat /sys/bus/usb/devices/1-7.1/devnum`
 sleep 7
 ../xc3sprog/build/xc3sprog -c xpc -m /opt/Xilinx/14.7/ISE_DS/ISE/xbr/data -v build/urukul.jed:w
